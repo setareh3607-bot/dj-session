21ddf9cad5e737e85c2b9f45d2308c5525322119
Tahereh Roudbari <setareh3607@gmail.com>
2026-07-05 12:15:10 +0330

    add models session

--- session/models.py
+++ session/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Session(models.Model):
+    session_key = models.CharField(max_length=256, unique=True)
+    session_data = models.TextField()
+    expire_date = models.DateTimeField()
