72a7a20e408477df32638968d4b536430946fb88
Tahereh Roudbari <setareh3607@gmail.com>
2026-07-15 15:55:45 +0330

    add

---
+++ session/middleware.py
@@ -0,0 +1,9 @@
+class SimpleMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        
+    def __call__(self, request):
+        
+        cookies = request.COOKIES.get("sessionid")
+        response = self.get_response(request)
+        return response
